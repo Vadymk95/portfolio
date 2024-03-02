32f2aeffe16291f4cdccf84a4bd69001d5ea15fd
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-03-03 00:42:27 +0200

    updated styles/components/assets

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.scss';
 
 export const Footer: FC = () => {
   return (
-    <footer className="footer">
+    <footer className="footer header-footer">
       <p>© 2024. All rights reserved.</p>
     </footer>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,23 @@
 import { FC } from 'react';
+import logo from '@/assets/images/logo.png';
 
 import './Header.scss';
 
 export const Header: FC = () => {
   return (
-    <header className="header">
-      <div className="logo">My Brand</div>
+    <header className="header header-footer">
+      <div className="logo">
+        <img src={logo} alt="Logo" className="logo-img" />
+      </div>
       <ul className="nav-links">
         <li>
-          <a href="#about">About</a>
+          <a href="#about">About me</a>
         </li>
         <li>
           <a href="#portfolio">Portfolio</a>
         </li>
         <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#testimonials">Testimonials</a>
+          <a href="#portfolio">What can I do?</a>
         </li>
         <li>
           <a href="#contact">Contact</a>
