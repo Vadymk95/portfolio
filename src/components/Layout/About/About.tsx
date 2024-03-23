255dedd1f588e4a24bf2d66ea4efa69957e433da
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-03-24 00:05:37 +0200

    created a paragraph for the 'About me' section

--- src/components/Layout/About/About.tsx
+++ src/components/Layout/About/About.tsx
@@ -8,7 +8,12 @@ export const About: FC = () => {
   return (
     <section className="about section">
       <Element name={Links.about.target} />
-      <h3 className="about__title">About me</h3>
+      <h3 className="about__title">About Me</h3>
+      <div className="about__text-container">
+        <p className="about__text wix-madefor">
+          Specializing in responsive design and SPAs
+        </p>
+      </div>
     </section>
   );
 };
